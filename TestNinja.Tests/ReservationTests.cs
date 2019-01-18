1204dce13ea8cc2f314112800b55795d6510982f
briang123 <briang.sandiego@gmail.com>
2019-01-18 14:41:32 -0800

    Add test project and create initial ReservationTests using MS Test Tools

---
+++ TestNinja.Tests/ReservationTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            var reservation = new Reservation();
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+
+
+    }
+}
