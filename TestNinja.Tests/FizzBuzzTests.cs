5c2aae43b4cd75908dc26162212a0e6e50ad98f4
briang123 <briang.sandiego@gmail.com>
2019-01-19 11:07:39 -0800

    fizz buzz tests

---
+++ TestNinja.Tests/FizzBuzzTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(-3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(0, "FizzBuzz")]
+        [TestCase(null, "FizzBuzz")]
+        public void GetOutput_WhenCalled_ReturnsCorrectFizzBuzzString(int number, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
