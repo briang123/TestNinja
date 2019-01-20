872aca0d97ce26bb82a3fc2cefc7b5edab439583
briang123 <briang.sandiego@gmail.com>
2019-01-20 10:49:03 -0800

    refactor and test InstallHelper class

---
+++ TestNinja.Tests/Mocking/InstallHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    internal class InstallHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd =>
+                fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+
+    }
+}
--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -1,27 +1,33 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace TestNinja.Mocking
 {
     public class InstallerHelper
     {
+        private readonly IFileDownloader _fileDownloader;
         private string _setupDestinationFile;
+
+        public InstallerHelper(IFileDownloader fileDownloader)
+        {
+            _fileDownloader = fileDownloader;
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
-            var client = new WebClient();
             try
             {
-                client.DownloadFile(
-                    string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
+                _fileDownloader.DownloadFile(
+                    String.Format("http://example.com/{0}/{1}",
+                            customerName,
+                            installerName),
                     _setupDestinationFile);
 
                 return true;
             }
             catch (WebException)
             {
-                return false; 
+                return false;
             }
         }
     }
