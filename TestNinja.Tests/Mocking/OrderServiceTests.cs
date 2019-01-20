26a5ece1428fb706aaa8543077c5d1ac0943f1ea
briang123 <briang.sandiego@gmail.com>
2019-01-20 08:33:23 -0800

    mock order service

---
+++ TestNinja.Tests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        private Mock<IStorage> _storage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _storage = new Mock<IStorage>();
+        }
+
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var service = new OrderService(_storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            _storage.Verify(s => s.Store(order));
+        }
+
+
+    }
+}
