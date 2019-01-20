f1d478210c8d84eebf158a01db5bfed6a7258952
briang123 <briang.sandiego@gmail.com>
2019-01-20 09:19:39 -0800

    add tests for Products

---
+++ TestNinja.Tests/Mocking/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    internal class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+
+        // DEMONSTRATING WHAT **NOT** TO DO - MORE COMPLEX
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount_DONOTMOCKLIKETHIS()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(c => c.IsGold).Returns(true);
+
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
--- TestNinja/Mocking/Product.cs
+++ TestNinja/Mocking/Product.cs
@@ -11,9 +11,24 @@
 
             return ListPrice;
         }
+
+        // TO DEMONSTRATE WHAT NOT TO DO -- MOCKING UNNECESSARILY
+        public float GetPrice(ICustomer customer)
+        {
+            if (customer.IsGold)
+                return ListPrice * 0.7f;
+
+            return ListPrice;
+        }
+
     }
 
-    public class Customer
+    public interface ICustomer
+    {
+        bool IsGold { get; set; }
+    }
+
+    public class Customer : ICustomer
     {
         public bool IsGold { get; set; }
     }
